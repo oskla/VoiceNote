59302d0a213fd5b06a1b5f4ca9918a656d33c0d9
oskla <oskarlarssson@gmail.com>
2022-10-13 12:43:39 +0200

    Initial Commit

---
+++ VoiceNote/VoiceNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceNoteApp.swift
+//  VoiceNote
+//
+//  Created by Oskar Larsson on 2022-10-13.
+//
+
+import SwiftUI
+
+@main
+struct VoiceNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
